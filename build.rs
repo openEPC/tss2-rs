d0d3078c94aa12107fa416770c40b3e48726fbd5
Iegor Sergieienkov <erepb1991@gmail.com>
2022-04-12 16:44:56 +0400

    allow tss lib selection

--- build.rs
+++ build.rs
@@ -3,16 +3,33 @@ extern crate bindgen;
 use std::env;
 use std::path::PathBuf;
 
+macro_rules! add_header {
+    ($builder:ident, $header:literal) => {
+        $builder = $builder.header($header);
+    }
+}
+
 fn main() {
     println!("cargo:rustc-link-lib=tss2-rc");
-    println!("cargo:rustc-link-lib=tss2-esys");
     println!("cargo:rustc-link-lib=tss2-mu");
+    #[cfg(feature = "sys")]
+    println!("cargo:rustc-link-lib=tss2-sys");
+    #[cfg(feature = "esys")]
+    println!("cargo:rustc-link-lib=tss2-esys");
+    #[cfg(feature = "fapi")]
     println!("cargo:rustc-link-lib=tss2-fapi");
 
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+    let mut builder = bindgen::Builder::default()
+        .header("wrapper.h");
+    #[cfg(feature = "sys")]
+    add_header!(builder, "sys.h");
+    #[cfg(feature = "esys")]
+    add_header!(builder, "esys.h");
+    #[cfg(feature = "fapi")]
+    add_header!(builder, "fapi.h");
+    let bindings = builder
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
